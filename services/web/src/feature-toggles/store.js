745ca7836ac4b1c0b59bcaa413c188b18db7723e
saadullah01 <saadullah1912@gmail.com>
2021-02-01 10:10:56 +0000

    feature toggle store created

---
+++ services/web/src/feature-toggles/store.js
@@ -0,0 +1,17 @@
+import { writable } from "svelte/store";
+
+const createStore = () => {
+  const { subscribe, update } = writable({});
+
+  return {
+    subscribe,
+    setFeatureToggle: (featureToggleName, isEnabled) =>
+      update(store => ({
+      ...store,
+      [featureToggleName]: isEnabled
+    }))
+  };
+};
+
+export const featureTogglesStore = createStore();
+
