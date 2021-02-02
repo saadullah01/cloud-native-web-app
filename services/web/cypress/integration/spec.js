65c42d6c8a1215b25b950e0f830ce37bc3bad79b
saadullah01 <saadullah1912@gmail.com>
2021-02-02 05:46:29 +0000

    added test using Testing Library

--- services/web/cypress/integration/spec.js
+++ services/web/cypress/integration/spec.js
@@ -8,3 +8,12 @@ describe('Header Test', () => {
   });
 });
 
+describe("Sapper template app", () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+  });
+
+  it("has the correct heading", () => {
+   cy.findByRole("heading").should("contain", "Great success!");
+  });
+});
